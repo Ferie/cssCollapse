958505ce016e2ce22741ac5cc8b4e76bdf9bcb1a
Riccardo Andreatta <riccardo.andreatta@myoffersltd.co.uk>
2015-09-02 15:51:59 +0100

    plugin with icon fonts running - accordion in to do

--- js/jquery.cssCollapse.js
+++ js/jquery.cssCollapse.js
@@ -4,6 +4,8 @@
 
 	var pluginName = 'cssCollapse',
 		defaults = {
+			accordion: 'yes',
+			accordionContainer: 'accordionContainer',
 			prefix: 'cssCollapse-',
 			targetClass: 'target',
 			hiddenContentClass: 'hiddenContent',
@@ -30,10 +32,53 @@
 
 	plugin.prototype = {
 		constructor: plugin,
-		toggleCollapse: function (event) {
-			var pluginThis = event.data.pluginThis,
-				$currentTarget = $(this),
+		toggleCollapse: function ($currentTarget) {
+			var pluginThis = this,
 				$collapseIcons = $currentTarget.find('.' + pluginThis.options.prefix + pluginThis.options.iconClass),
+				$hiddenContent = $currentTarget.siblings('.' + pluginThis.options.prefix + pluginThis.options.hiddenContentClass);
+
+			if ($hiddenContent.hasClass(pluginThis.options.collapseClass)) {
+				$hiddenContent.removeClass(pluginThis.options.collapseClass);
+				$collapseIcons.removeClass(pluginThis.options.prefix + pluginThis.options.iconClose).addClass(pluginThis.options.prefix + pluginThis.options.iconOpen);
+				$(document).trigger(pluginThis._name + '.close');
+			} else {
+				$hiddenContent.addClass(pluginThis.options.collapseClass);
+				$collapseIcons.removeClass(pluginThis.options.prefix + pluginThis.options.iconOpen).addClass(pluginThis.options.prefix + pluginThis.options.iconClose);
+				$(document).trigger(pluginThis._name + '.open');
+			}
+		},
+		accordionCollapse: function ($currentTarget) {
+			var pluginThis = this,
+				$collapseIcons = $currentTarget.find('.' + pluginThis.options.prefix + pluginThis.options.iconClass),
+				$hiddenContent = $currentTarget.siblings('.' + pluginThis.options.prefix + pluginThis.options.hiddenContentClass),
+				$currentParent = $currentTarget.parents('.' + pluginThis.options.accordionContainer),
+				$accordionCollapseIcons = $currentParent.find('.' + pluginThis.options.prefix + pluginThis.options.targetClass + ' > .' + pluginThis.options.prefix + pluginThis.options.iconClass),
+				$accordionHiddenContent = $currentParent.find('.' + pluginThis.options.prefix + pluginThis.options.hiddenContentClass);
+
+			if ($hiddenContent.hasClass(pluginThis.options.collapseClass)) {
+				$hiddenContent.removeClass(pluginThis.options.collapseClass);
+				$collapseIcons.removeClass(pluginThis.options.prefix + pluginThis.options.iconClose).addClass(pluginThis.options.prefix + pluginThis.options.iconOpen);
+				$(document).trigger(pluginThis._name + '.accordionClose');
+			} else {
+				$accordionHiddenContent.removeClass(pluginThis.options.collapseClass);
+				$accordionCollapseIcons.removeClass(pluginThis.options.prefix + pluginThis.options.iconClose);
+				$hiddenContent.addClass(pluginThis.options.collapseClass);
+				$collapseIcons.addClass(pluginThis.options.prefix + pluginThis.options.iconClose);
+				$(document).trigger(pluginThis._name + '.accordionOpen');
+			}
+		},
+		checkAccordion: function (event) {
+			var pluginThis = event.data.pluginThis,
+				$currentTarget = $(this);
+			pluginThis.behaviour($currentTarget);
+			if (pluginThis.options.accordion == 'yes') {
+				pluginThis.accordionCollapse($currentTarget);
+			} else {
+				pluginThis.toggleCollapse($currentTarget);
+			}
+		},
+		behaviour: function ($currentTarget) {
+			var pluginThis = this,
 				$hiddenContent = $currentTarget.siblings('.' + pluginThis.options.prefix + pluginThis.options.hiddenContentClass);
 
 			if (pluginThis.options.behaviour != '') {
@@ -65,19 +110,9 @@
 					'transition-delay': pluginThis.options.delay
 				});
 			}
-
-			if ($hiddenContent.hasClass(pluginThis.options.collapseClass)) {
-				$hiddenContent.removeClass(pluginThis.options.collapseClass);
-				$collapseIcons.removeClass(pluginThis.options.prefix + pluginThis.options.iconClose).addClass(pluginThis.options.prefix + pluginThis.options.iconOpen);
-				$(document).trigger(pluginThis._name + '.open');
-			} else {
-				$hiddenContent.addClass(pluginThis.options.collapseClass);
-				$collapseIcons.removeClass(pluginThis.options.prefix + pluginThis.options.iconOpen).addClass(pluginThis.options.prefix + pluginThis.options.iconClose);
-				$(document).trigger(pluginThis._name + '.close');
-			}
 		},
 		eventBindings: function () {
-			this.collapseHeadings.on('click', {pluginThis: this}, this.toggleCollapse);
+			this.collapseHeadings.on('click', {pluginThis: this}, this.checkAccordion);
 		},
 		init: function () {
 			this.eventBindings();
